67629a1ac4be9bedf2ec031f6426ab53c3ad8efb
Sanghyuk <saanghyuk@gmail.com>
2017-10-17 16:20:04 +0900

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,24 @@
-var socket= io();
+var socket = io();
 
-socket.on('connect', function(){
+socket.on('connect', function () {
     console.log('Connected to server');
 });
 
-socket.on('disconnect', function(){
+socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(message){
+socket.on('newMessage', function (message) {
     console.log('newMessage', message);
-    var li=jQuery('<li></li>');
+    var li = jQuery('<li></li>');
     li.text(`${message.from}: ${message.text}`);
 
-    jQuery('#messages').append(li)
+    jQuery('#messages').append(li);
 });
 
-socket.on('newLocationMessage', function(message){
+socket.on('newLocationMessage', function (message) {
     var li = jQuery('<li></li>');
-    var a=jQuery('<a target="__blank">My current location</a>');
+    var a = jQuery('<a target="_blank">My current location</a>');
 
     li.text(`${message.from}: `);
     a.attr('href', message.url);
@@ -26,29 +26,35 @@ socket.on('newLocationMessage', function(message){
     jQuery('#messages').append(li);
 });
 
-jQuery('#message-form').on('submit', function(e) {
-    e.preventDefault(); //page refresh되는 것 override
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    var messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('')
     });
 });
 
-var locationButton= jQuery('#send-location');
-locationButton.on('click', function(){
-    if(!navigator.geolocation){
-        return alert('Geolocation not supported by user browser.')
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser.');
     }
 
-    navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
-        })
-    }, function(){
+        });
+    }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
-    })
+    });
 });
